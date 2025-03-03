bce8f06428c206f50fb368f788634d905a23db4a
Djongoks <113566110+Djongoks@users.noreply.github.com>
2025-03-03 11:20:47 +0700

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright ©️ Rio • Rara 2025</p>
+    </footer>
+  );
+};
+
+export default Footer;
