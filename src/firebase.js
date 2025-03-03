c2b661f3ca59f7e2448b558c253ddce93f00263b
Djongoks <113566110+Djongoks@users.noreply.github.com>
2025-03-03 12:35:21 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore"; 
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
+// Konfigurasi Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCkfNpj5MzecbYmJx2_kftkJsuHuOlV1DI",
   authDomain: "web-musik-9056a.firebaseapp.com",
   projectId: "web-musik-9056a",
-  storageBucket: "web-musik-9056a.appspot.com",
+  storageBucket: "web-musik-9056a.appspot.com", // Perbaiki storage bucket
   messagingSenderId: "844922737017",
   appId: "1:844922737017:web:777cd3d33fa87acd665fa8",
   measurementId: "G-QBYPNWS0NW",
 };
 
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+// Inisialisasi Firebase Services
+const auth = getAuth(app); // Untuk Authentication
+const db = getFirestore(app); // Untuk Firestore Database
+const storage = getStorage(app); // Untuk menyimpan gambar
+
+export { auth, db, storage, analytics };
