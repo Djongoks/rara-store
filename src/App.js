e1fa3a9dde0c0a72f461b6a3a3853422554e6a36
Djongoks <113566110+Djongoks@users.noreply.github.com>
2025-03-03 12:22:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import ProductDetail from './pages/ProductDetail';
-import Admin from './pages/Admin';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Admin from "./pages/Admin";
+import AdminLogin from "./pages/AdminLogin"; // Tambahkan halaman login admin
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/admin-login" element={<AdminLogin />} /> {/* Rute login admin */}
       </Routes>
       <Footer />
     </Router>
