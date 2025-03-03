0cbc5359557be5c39c5c9248b4a05c4e75dc10e4
Djongoks <113566110+Djongoks@users.noreply.github.com>
2025-03-03 12:41:58 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail";
+import AdminLogin from "./pages/AdminLogin";
 import Admin from "./pages/Admin";
-import AdminLogin from "./pages/AdminLogin"; // Tambahkan halaman login admin
 import Footer from "./components/Footer";
 
 function App() {
+  const [user] = useAuthState(auth); // Cek user login
+
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/admin-login" element={<AdminLogin />} /> {/* Rute login admin */}
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin" element={user ? <Admin /> : <Navigate to="/admin-login" />} />
       </Routes>
       <Footer />
     </Router>
